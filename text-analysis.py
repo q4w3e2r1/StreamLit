99f8c474e859a12756f01c57a00e85425f9ae6b1
q4w3e2r1 <126683181+q4w3e2r1@users.noreply.github.com>
2025-05-31 15:46:02 +0500

    Update text-analysis.py

--- text-analysis.py
+++ text-analysis.py
@@ -4,7 +4,6 @@ from transformers import AutoModelForSequenceClassification, AutoTokenizer
 from huggingface_hub import hf_hub_download
 import time
 
-# Переносим класс TextClassifier прямо в основной файл
 class TextClassifier:
     def __init__(self, model_path):
         HF_MODEL_NAME = 'SeKooooo/project'
